a673c820983a3c0d1154224b9de12f4ecde53f6a
Pengfei Li <ldsoar1990@gmail.com>
2026-09-24 14:32:22 -0500

    Add a script for evalulating MACE energies.

---
+++ example-scripts/eval-mace.sh
@@ -0,0 +1,5 @@
+mace_eval_configs     --model="MACE_Zn18_v1_stagetwo.model"     --configs="mace_test_final.xyz"     --output="eval_test_results.xyz"     --info_prefix="MACE_"
+
+mace_eval_configs     --model="MACE_Zn18_v1_stagetwo.model"     --configs="mace_valid_final.xyz"     --output="eval_valid_results.xyz"     --info_prefix="MACE_"
+
+mace_eval_configs     --model="MACE_Zn18_v1_stagetwo.model"     --configs="mace_train_final.xyz"     --output="eval_train_results.xyz"     --info_prefix="MACE_"
